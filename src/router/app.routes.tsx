c53a359fc32650339afed1cb13a0bb8aa8f892ff
Leonardo <mr_kunkel@hotmail.com>
2026-02-26 15:09:29 -0600

    first commit

---
+++ src/router/app.routes.tsx
@@ -0,0 +1,50 @@
+import { AdminLayout } from "@/admin/layouts/AdminLayout";
+import { AdminPage } from "@/admin/pages/AdminPage";
+import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
+import { HeroPage } from "@/heroes/pages/hero/HeroPage";
+import { HomePage } from "@/heroes/pages/home/HomePage";
+import { lazy } from "react";
+// import { SearchPage } from "@/heroes/pages/search/SearchPage";
+import { createBrowserRouter, Navigate } from "react-router";
+
+const SearchPage = lazy(() => import('@/heroes/pages/search/SearchPage'));
+
+export const appRouter = createBrowserRouter([
+
+    {
+        path: '/',
+        element: <HeroesLayout />,
+        children: [
+            {
+                index: true,
+                element: <HomePage />
+            },
+            {
+                path: '/hero/:idSlug',
+                element: <HeroPage />
+            },
+            {
+                path: '/search',
+                element: <SearchPage />
+            },
+            {
+                // Esto es útil para que redireccione a una página existente
+                path: '*',
+                // element: <h1>404</h1>
+                element: <Navigate to='/' />
+            }
+        ]
+    },
+    {
+        path: '/admin',
+        element: <AdminLayout />,
+        children: [
+            {
+                index: true,
+                element: <AdminPage />
+            }
+        ]
+    },
+
+])
+
